e7a26b6eb0e2cb6d871fd622586a8f2ba7535ea0
Nico Sabena <nico.sabena@hotmail.com>
2019-02-08 08:07:52 -0300

    Added additional referrer.

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function(details) {
+    var offendingReferrers = ["https://auth0.lightning.force.com/", "https://auth0.my.salesforce.com/"]
     if (
       details.requestHeaders &&
       details.requestHeaders.some(
         // only if the request came from Salesforce
         header =>
           header.name === "Referer" &&
-          header.value === "https://auth0.my.salesforce.com/"
+          offendingReferrers.some( (referrer) => header.value.indexOf(referrer) > -1) 
       )
     ) {
       return { cancel: true };
